9d0bdf4fab5f1ad61f7e9711a3a3698749f94943
3dw1nM0535 <workockmoses@gmail.com>
2017-11-09 12:51:40 +0300

    configured react-apollo and apollo-client

--- src/index.js
+++ src/index.js
@@ -4,5 +4,28 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Apollo client configuration
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+//http configuration
+const httpLink = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cj9s7op957h810101moob03if' });
+
+//Apollo client interface configuration
+const client = new ApolloClient({
+  link: httpLink,
+  cache: InMemoryCache()
+});
+
+//REactDOM reconfigured for react-apollo client
+
+ReactDOM.render(
+  <ApolloClient client={client}>
+    <App />
+  </ApolloClient>,
+  
+  document.getElementById('root'));
+
 registerServiceWorker();
