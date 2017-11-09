8eb2fd06feeefe0d10f62f5f7a11b291429b878a
3dw1nM0535 <workockmoses@gmail.com>
2017-11-09 13:39:32 +0300

    communicated with GraphQL server to pull data and bind the data to react component using graphql

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,22 +1,21 @@
 import React, { Component } from 'react';
 import Link from './Link';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
 class LinkList extends Component {
 
   render() {
 
-    const linksToRender = [
-      {
-        id: '1',
-        description: 'The Collest GraphQL Backend',
-        url: 'https://www.graphcool.com'
-      },
-      {
-        id: '2',
-        description: 'The Best GraphQL Client',
-        url: 'https://dev.apollodata.com/'
-      }
-    ];
+    if (this.props.allLinksQuery && this.props.allLinksQuery.loading) {
+      return <div>Loading....</div>
+    }
+
+    if (this.props.allLinksQuery && this.props.allLinksQuery.error) {
+      return <div>Error!</div>
+    }
+
+    const linksToRender = this.props.allLinksQuery.allLinks;
 
     return (
       <div>
@@ -30,4 +29,17 @@ class LinkList extends Component {
   }
 }
 
-export default LinkList;
+//setup query fetching schema
+const ALL_LINKS_QUERY = gql`
+  
+  query AllLinksQuery {
+    allLinks {
+      id
+      createdAt
+      description
+      url
+    }
+  }
+`
+
+export default graphql(ALL_LINKS_QUERY, { name: 'allLinksQuery' })(LinkList);
